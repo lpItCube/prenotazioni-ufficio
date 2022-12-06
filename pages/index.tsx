b252ea2873ed2d584551f6dda99b21f787e8e913
Luca Pettenella <l.pettenella@reply.it>
2022-12-06 16:31:14 +0100

    add: relation between user and seat

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,25 +9,33 @@ export default Home
 
 export const getStaticProps: GetStaticProps = async () => {
   await prisma.seat.deleteMany()
+  await prisma.user.deleteMany()
+
+  const user = await prisma.user.create(
+    {
+      data: { username: "admin" }    
+    }
+  )
+
   await prisma.seat.createMany( 
     {
       data: [
-        { name: "it-1", type: "it", busy: false },
-        { name: "it-2", type: "it", busy: false },
-        { name: "it-3", type: "it", busy: false },
-        { name: "it-4", type: "it", busy: true },
-        { name: "it-5", type: "it", busy: false },
-        { name: "it-6", type: "it", busy: false },
-        { name: "it-7", type: "it", busy: false },
-        { name: "it-8", type: "it", busy: true },
-        { name: "meet-1", type: "meet", busy: false },
-        { name: "meet-2", type: "meet", busy: false },
-        { name: "meet-3", type: "meet", busy: false },
-        { name: "meet-4", type: "meet", busy: true },
-        { name: "meet-5", type: "meet", busy: false },
-        { name: "meet-6", type: "meet", busy: false },
-        { name: "meet-7", type: "meet", busy: false },
-        { name: "meet-8", type: "meet", busy: true }
+        { name: "it-1", type: "it", busy: false, userId: user.id },
+        { name: "it-2", type: "it", busy: false, userId: user.id },
+        { name: "it-3", type: "it", busy: false, userId: user.id },
+        { name: "it-4", type: "it", busy: true, userId: user.id },
+        { name: "it-5", type: "it", busy: false, userId: user.id },
+        { name: "it-6", type: "it", busy: false, userId: user.id },
+        { name: "it-7", type: "it", busy: false, userId: user.id },
+        { name: "it-8", type: "it", busy: true, userId: user.id },
+        { name: "meet-1", type: "meet", busy: false, userId: user.id },
+        { name: "meet-2", type: "meet", busy: false, userId: user.id },
+        { name: "meet-3", type: "meet", busy: false, userId: user.id },
+        { name: "meet-4", type: "meet", busy: true, userId: user.id },
+        { name: "meet-5", type: "meet", busy: false, userId: user.id },
+        { name: "meet-6", type: "meet", busy: false, userId: user.id },
+        { name: "meet-7", type: "meet", busy: false, userId: user.id },
+        { name: "meet-8", type: "meet", busy: true, userId: user.id }
       ]
     }
   )
