5b261671002ccb9e73fefbc22fe6d5f5250e0370
unknown <matteo.oliveri@itcubeconsulting.it>
2023-01-25 17:33:22 +0100

    save

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const { pathname } = request.nextUrl;
   const protectedPaths = [
     "/prenota", 
-    "/api/reserve/", 
+    "/api/reserve", 
     "/api/reserve/:id", 
     "/api/reserve/approveReserve",
     "/api/reserve/pending",
@@ -13,9 +13,11 @@ export async function middleware(request: NextRequest, _next: NextFetchEvent) {
     "/api/users/:username",
     "/api/addReserve"
   ];
-  const matchesProtectedPath = protectedPaths.some((path) =>
+  const matchesProtectedPath = protectedPaths.some((path:any) =>
     pathname.startsWith(path)
   );
+
+  console.log('METCH',matchesProtectedPath)
 
   if (matchesProtectedPath) {
     const token = await getToken({ req: request });
@@ -35,3 +37,47 @@ export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   }
   return NextResponse.next();
 }
+
+
+
+
+
+
+// import { withAuth } from "next-auth/middleware"
+// import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+
+
+// export default withAuth(
+//   // `withAuth` augments your `Request` with the user's token.
+//   async function middleware(req) {
+//     const token = req.cookies.get('next-auth.session-token')?.value
+//     // const getTokenaw = await getToken({ req: req });
+//     // console.log('TOKENTOKEN',getTokenaw)
+//     // console.log('REQ COO',req.cookies === null)
+//     // console.log('REQUEST MIDDLEWARE',req.cookies.get('next-auth.csrf-token')?.value)
+//     // console.log('PASS MIDDLEWARE', req.cookies.get('next-auth.session-token')?.value)
+//     if(!token) {
+//         return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
+//     }
+// },
+//   {
+//     callbacks: {
+//       authorized: ( req:any ) => req.cookies !== null,
+//     //   authorized: ({ token }) => true,
+//     },
+//   }
+// )
+
+// export const config = { matcher: [
+//     "/prenota", 
+//     "/api/reserve/", 
+//     "/api/reserve/:id", 
+//     "/api/reserve/approveReserve",
+//     "/api/reserve/pending",
+//     "/api/seats/:seatName",
+//     "/api/userReserves/:userId",
+//     "/api/users/:username",
+//     "/api/addReserve"
+// ] }
+
