bd2e008947507c6f89c86f012474c1d467514090
unknown <matteo.oliveri@itcubeconsulting.it>
2023-01-25 17:18:08 +0100

    save

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,14 @@ import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
   const { pathname } = request.nextUrl;
-  const protectedPaths = ["/prenota"];
+  const protectedPaths = ["/prenota", "/secure"];
   const matchesProtectedPath = protectedPaths.some((path) =>
     pathname.startsWith(path)
   );
 
   if (matchesProtectedPath) {
     const token = await getToken({ req: request });
+    console.log('TOKEN',token)
     if (!token) {
       const url = new URL(`/login`, request.url);
       url.searchParams.set("callbackUrl", encodeURI(request.url));
