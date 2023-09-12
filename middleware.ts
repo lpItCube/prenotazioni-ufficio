0c47dd77b5f3725c2b702ae8639bb13dd25c4c79
Matteo Oliveri <matteo.oliveri@itcubeconsulting.it>
2023-09-12 09:13:24 +0200

    Remove callback url and redirect to login

--- middleware.ts
+++ middleware.ts
@@ -1,55 +1,55 @@
-
 import { current } from "@reduxjs/toolkit";
 import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest, _next: NextFetchEvent) {
-  const { pathname } = request.nextUrl
-  const superadminPaths = ["/create/user", "/api/users"]
-  const adminPaths = ["/secure", "/prenotazioni/pending", "/create/room"]
-  const loggedPaths = [...adminPaths, ...superadminPaths, "/prenota", "/prenotazioni"]
-  // console.log(await getToken({ req: request }))
+	const { pathname } = request.nextUrl;
+	const superadminPaths = ["/create/user", "/api/users"];
+	const adminPaths = ["/secure", "/prenotazioni/pending", "/create/room"];
+	const loggedPaths = [
+		...adminPaths,
+		...superadminPaths,
+		"/prenota",
+		"/prenotazioni",
+	];
+	// console.log(await getToken({ req: request }))
 
-  // Prendere una rotta all volta, altrimenti il sistema fa il redirect alla login
-  const currentPath = loggedPaths.filter((curr: any) => pathname.includes(curr))
+	// Prendere una rotta all volta, altrimenti il sistema fa il redirect alla login
+	const currentPath = loggedPaths.filter((curr: any) =>
+		pathname.includes(curr)
+	);
 
-  const loggedInPath = currentPath.some((path) =>
-    pathname.includes(path)
-  );
+	const loggedInPath = currentPath.some((path) => pathname.includes(path));
 
-  const adminPath = adminPaths.some((path) =>
-    pathname.includes(path)
-  )
+	const adminPath = adminPaths.some((path) => pathname.includes(path));
 
-  const superadminPath = superadminPaths.some((path) => 
-    pathname.includes(path)
-  )
-    
-  if (currentPath.length > 0) {
-    const token = await getToken({ req: request });
-    if (!token && loggedInPath) {
-      if (pathname.includes('api')) {
-        const url = new URL(`/api/auth/unauthorized`, request.url);
-        return NextResponse.redirect(url);
-      } else {
-        const url = new URL(`/login`, request.url);
-        url.searchParams.set("callbackUrl", encodeURI(request.url));
-        return NextResponse.redirect(url);
-      }
-    }
-    else if (token && adminPath) {
-      if (token.role !== "ADMIN" && token.role !== "SUPERADMIN") {
-        const url = new URL(`/403`, request.url);
-        return NextResponse.rewrite(url);
-      }
-    } 
-    else if (token && superadminPath) {
-      if (token.role !== "SUPERADMIN") {
-        const url = new URL(`/403`, request.url);
-        return NextResponse.rewrite(url);
-      }
-    }
-  } else {
-    return NextResponse.next();
-  }
+	const superadminPath = superadminPaths.some((path) =>
+		pathname.includes(path)
+	);
 
+	if (currentPath.length > 0) {
+		const token = await getToken({ req: request });
+		if (!token && loggedInPath) {
+			if (pathname.includes("api")) {
+				const url = new URL(`/api/auth/unauthorized`, request.url);
+				return NextResponse.redirect(url);
+			} else {
+				const url = new URL(`/login`, request.url);
+				// REMOVED BECAUSE WORK BAD ON VERCEL
+				// url.searchParams.set("callbackUrl", encodeURI(request.url));
+				return NextResponse.redirect(url);
+			}
+		} else if (token && adminPath) {
+			if (token.role !== "ADMIN" && token.role !== "SUPERADMIN") {
+				const url = new URL(`/403`, request.url);
+				return NextResponse.rewrite(url);
+			}
+		} else if (token && superadminPath) {
+			if (token.role !== "SUPERADMIN") {
+				const url = new URL(`/403`, request.url);
+				return NextResponse.rewrite(url);
+			}
+		}
+	} else {
+		return NextResponse.next();
+	}
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,84 +1,89 @@
-import { FormEventHandler, useState } from "react"
-import { signIn } from "next-auth/react"
-import { useRouter } from "next/router"
-import { useSession } from "next-auth/react"
+import { FormEventHandler, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 // Components
-import ErrorAlert from '../components/login/ErrorAlert'
-import LoginForm from '../components/login/LoginForm'
+import ErrorAlert from "../components/login/ErrorAlert";
+import LoginForm from "../components/login/LoginForm";
 import Logo from "../components/Ui/Logo";
-import Spinner from "../components/Ui/Spinner"
-import { AUTH_OK } from "../_shared"
+import Spinner from "../components/Ui/Spinner";
+import { AUTH_OK } from "../_shared";
 
 function Login() {
+	const [userInfo, setUserInfo] = useState<{
+		email: string;
+		password: string;
+	}>({ email: "", password: "" });
+	const [loginError, setLoginError] = useState<boolean>(false);
+	const [loadingLogin, setLoadingLogin] = useState<boolean>(false);
+	const [enterFromLogin, setEnterFromLogin] = useState<boolean>(false);
 
-	const [userInfo, setUserInfo] = useState<{email:string, password:string}>({ email: "", password: "" })
-	const [loginError, setLoginError] = useState<boolean>(false)
-	const [loadingLogin, setLoadingLogin] = useState<boolean>(false)
-	const [enterFromLogin, setEnterFromLogin] = useState<boolean>(false)
-
-	const router = useRouter()
-	const session = useSession()
-
+	const router = useRouter();
+	const session = useSession();
 
 	const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-		e.preventDefault()
+		e.preventDefault();
 
-		setEnterFromLogin(true)
-		setLoadingLogin(true)
-		setLoginError(false)
-
+		setEnterFromLogin(true);
+		setLoadingLogin(true);
+		setLoginError(false);
 
 		const res = await signIn("credentials", {
 			email: userInfo.email,
 			password: userInfo.password,
-			redirect: false
-		})
+			redirect: false,
+		});
 
-
-		const urlToEncode: string | undefined = res?.url?.split('callbackUrl=')[1]
+		// const urlToEncode: string | undefined = res?.url?.split('callbackUrl=')[1]
 
 		if (res && res!.error) {
-			setLoginError(true)
-			setLoadingLogin(false)
+			setLoginError(true);
+			setLoadingLogin(false);
 		}
 
-		else urlToEncode
-			? router.push(decodeURIComponent(urlToEncode))
-			: router.push('/prenota')
-	}
+		// REMOVED BECAUSE WORK BAD ON VERCEL
+		// else urlToEncode
+		// 	? router.push(decodeURIComponent(urlToEncode))
+		// 	: router.push('/prenota')
+		else {
+			router.push("/prenota");
+		}
+	};
 
 	if (session.status === AUTH_OK && !enterFromLogin) {
-		router.push('/prenota')
-		return
+		router.push("/prenota");
+		return;
 	}
 
-	let loginForm
+	let loginForm;
 
-	if (session.status === 'loading') {
-		loginForm = <Spinner />
+	if (session.status === "loading") {
+		loginForm = <Spinner />;
 	} else {
-		loginForm = <LoginForm
-			handleSubmit={handleSubmit}
-			setUserInfo={setUserInfo}
-			userInfo={userInfo}
-			isLoading={loadingLogin}
-		/>
+		loginForm = (
+			<LoginForm
+				handleSubmit={handleSubmit}
+				setUserInfo={setUserInfo}
+				userInfo={userInfo}
+				isLoading={loadingLogin}
+			/>
+		);
 	}
 	return (
 		<div className="loginContainer">
 			<div className="loginModal">
 				<Logo />
-				{loginError &&
+				{loginError && (
 					<ErrorAlert
-						title={'Credenziali non valide'}
-						text={'Inserisci username e password corretti'}
+						title={"Credenziali non valide"}
+						text={"Inserisci username e password corretti"}
 					/>
-				}
+				)}
 				{loginForm}
 			</div>
 		</div>
-	)
+	);
 }
 
-export default Login
+export default Login;
